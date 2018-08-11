856e8ed1cfd176184371a101775c53a8870ef21e
matesebi <matesebi96@gmail.com>
2018-08-11 22:27:30 +0200

    Initial commit

---
+++ src/main/java/mate_sebestyen/test/blogengine/BlogengineApplication.java
@@ -0,0 +1,12 @@
+package mate_sebestyen.test.blogengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogengineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogengineApplication.class, args);
+    }
+}
---
+++ src/test/java/mate_sebestyen/test/blogengine/BlogengineApplicationTests.java
@@ -0,0 +1,16 @@
+package mate_sebestyen.test.blogengine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlogengineApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
