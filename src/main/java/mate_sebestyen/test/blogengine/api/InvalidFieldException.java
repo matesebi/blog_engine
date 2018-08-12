d0c697bf74757a6bf84ffb92796adf00d225673d
matesebi <matesebi96@gmail.com>
2018-08-12 16:19:32 +0200

    Renamed to represent only a single field was not found

--- src/main/java/mate_sebestyen/test/blogengine/api/InvalidFieldException.java
+++ src/main/java/mate_sebestyen/test/blogengine/api/InvalidFieldException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-class InvalidFieldsException extends RuntimeException {
-    InvalidFieldsException(String field) {
+class InvalidFieldException extends RuntimeException {
+    InvalidFieldException(String field) {
         super("The following field is null or empty: '" + field + "'");
     }
 }
