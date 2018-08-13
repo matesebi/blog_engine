79482c8f8189d7f23c08116f094bca58e569ea58
matesebi <matesebi96@gmail.com>
2018-08-13 21:16:00 +0200

    Removed unused query

--- src/main/java/mate_sebestyen/test/blogengine/dal/BlogPostRepository.java
+++ src/main/java/mate_sebestyen/test/blogengine/dal/BlogPostRepository.java
@@ -6,10 +6,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Collection;
-
 public interface BlogPostRepository extends JpaRepository<BlogPost, Long> {
-    Collection<BlogPost> findByTagsIsContaining(Tag tag);
-
     Page<BlogPost> findAllByTagsContaining(Tag tag, Pageable pageable);
 }
